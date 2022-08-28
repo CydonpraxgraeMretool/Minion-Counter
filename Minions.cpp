d3171e7ecd4a9eafe5aa1bc5fbbd80ab3580e50f
CydonpraxgraeMretool <87585218+CydonpraxgraeMretool@users.noreply.github.com>
2022-08-27 23:48:21 -0400

    Add files via upload

---
+++ Minions.cpp
@@ -0,0 +1,19 @@
+//A program to see how many minions you have
+#include <iostream>
+
+int  main(void)
+
+{
+	using namespace std;
+
+	float ym, pm, m;
+	
+	cout << "How many Yellow Minions do you have ";
+	cin >> ym;
+	cout << "How many Purple Minions do you have ";
+	cin >> pm;
+	m = ym + pm;
+	cout << m << endl;
+
+}
+
